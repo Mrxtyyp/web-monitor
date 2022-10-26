9e235e8b86c0e3d8776a25cd35fb8cbfe5e1c6d1
Mrxtyyp <1126084777@qq.com>
2022-10-26 09:44:07 +0800

    feat: 新加报告发送

--- src/utils/report.ts
+++ src/utils/report.ts
@@ -18,12 +18,30 @@ function getCommonData(): ReportCommonData {
     };
 }
 
+/**
+ * 向下兼容发送信号的方法
+ */
+ const sendReport = navigator.sendBeacon
+ ? (url: string, data: any) => {
+   if (data) navigator.sendBeacon(url, JSON.stringify(data));
+ }
+ : (url: string, data: any) => {
+   // 传统方式传递参数
+   const beacon = new Image();
+   beacon.src = `${url}?v=${encodeURIComponent(JSON.stringify(data))}`;
+ };
+
 const send = (data: ReportErrorData) => {
     const common = getCommonData()
     console.log({
         ...common,
         ...data
     });
+
+    sendReport(monitorSdk.config.target, {
+        ...common,
+        ...data
+    })
 }
 
 export default {
